e3b7722eff1af0f3ebb2360717147eb01aa3898d
rishesh007 <risheshagarwala1@gmail.com>
2019-08-11 23:00:03 +0530

    pipe for html

--- pipe.js
+++ pipe.js
@@ -1,15 +1,13 @@
 var fs = require('fs');
 var http = require('http');
 
-
-
 var server  = http.createServer(function(req,res){
     console.log('request was made : ' + req.url);
-    res.writeHead(200,{'Content-Type': 'text/plain'});
-    var read = fs.createReadStream(__dirname + '/read.txt','utf8');
+    res.writeHead(200,{'Content-Type': 'text/html'});
+    var read = fs.createReadStream(__dirname + '/index.html','utf8');
     // var write = fs.createWriteStream(__dirname + '/write.txt');
     read.pipe(res);
 });
 
-server.listen(3000,'127.0.0.1');
+server.listen(5000,'127.0.0.1');
 console.log('Bahut hard!!');
