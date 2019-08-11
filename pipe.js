c942fba5530e08e795bc3486b26307956df95bb1
rishesh007 <risheshagarwala1@gmail.com>
2019-08-11 23:22:27 +0530

    basic routing

--- pipe.js
+++ pipe.js
@@ -3,17 +3,24 @@ var http = require('http');
 
 var server  = http.createServer(function(req,res){
     console.log('request was made : ' + req.url);
-    res.writeHead(200,{'Content-Type': 'application/json'});
-    // var read = fs.createReadStream(__dirname + '/index.html','utf8');
-    // var write = fs.createWriteStream(__dirname + '/write.txt');
-    // read.pipe(res);
-    var myobj = {
-        name : 'Bolt',
-        job : 'Student',
-        age : 20
+    if(req.url === '/' || req.url === '/home'){
+        res.writeHead(200,{'Content-Type' : 'text/html'});
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
     }
-    res.end(JSON.stringify(myobj));
+    else if(req.url === '/contact'){
+        res.writeHead(200,{'Content-Type' : 'text/html'});
+        fs.createReadStream(__dirname + '/contact.html').pipe(res);
+    }
+    else if(req.url == '/api/bolt'){
+        var names = [{names : 'bolt', age : 20},{name : 'zerif',age : 21}];
+        res.writeHead(200,{'Content-Type' : 'application/json'});
+        res.end(JSON.stringify(names));
+    }
+    else{
+        res.writeHead(404,{'Content-Type' : 'text/html'});
+        fs.createReadStream(__dirname + '/error.html').pipe(res);
+    }
 });
 
-server.listen(5100,'127.0.0.1');
+server.listen(3002,'127.0.0.1');
 console.log('Bahut hard!!');
