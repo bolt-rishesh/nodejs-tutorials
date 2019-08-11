78102af90046b8d87be061c939ff9fe061e3d1fd
rishesh007 <risheshagarwala1@gmail.com>
2019-08-11 23:05:46 +0530

    pipe for json

--- pipe.js
+++ pipe.js
@@ -3,11 +3,17 @@ var http = require('http');
 
 var server  = http.createServer(function(req,res){
     console.log('request was made : ' + req.url);
-    res.writeHead(200,{'Content-Type': 'text/html'});
-    var read = fs.createReadStream(__dirname + '/index.html','utf8');
+    res.writeHead(200,{'Content-Type': 'application/json'});
+    // var read = fs.createReadStream(__dirname + '/index.html','utf8');
     // var write = fs.createWriteStream(__dirname + '/write.txt');
-    read.pipe(res);
+    // read.pipe(res);
+    var myobj = {
+        name : 'Bolt',
+        job : 'Student',
+        age : 20
+    }
+    res.end(JSON.stringify(myobj));
 });
 
-server.listen(5000,'127.0.0.1');
+server.listen(5100,'127.0.0.1');
 console.log('Bahut hard!!');
