59394ea897ca22f98c9e2ce7979f41b0194b0d81
rishesh007 <risheshagarwala1@gmail.com>
2019-08-12 18:33:52 +0530

    ejs files

--- app1.js
+++ app1.js
@@ -2,17 +2,19 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine','ejs');
+
 app.get('/',function(req,res){
-    res.sendFile(__dirname + '/index.html');
+    res.render('index');
 });
 
 app.get('/contact',function(req,res){
-    res.sendFile(__dirname + '/contact.html');
+    res.render('contact');
 });
 
 
-app.get('/profile/:id',function(req,res){
-    res.send('you requested to see a profile with the id of ' + req.params.id);
+app.get('/profile/:name',function(req,res){
+    res.render('profile',{person: req.params.name});
 });
 
-app.listen(3003);
+app.listen(3008);
